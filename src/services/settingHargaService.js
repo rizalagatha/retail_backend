9350738010aed3475f09fd818bc42054f34ad55d
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-09-06 10:35:08 +0700

    Menu Baru Setting Harga

--- src/services/settingHargaService.js
+++ src/services/settingHargaService.js
@@ -5,19 +5,21 @@ const pool = require('../config/database');
  * Menerjemahkan query dari btnRefreshClick di Delphi.
  */
 const getAllTshirtTypes = async () => {
-  const query = `
+    const query = `
     SELECT 
       j.jk_Jenis AS JenisKaos,
       j.jk_custom AS Custom,
       j.jk_s AS Harga_S, j.jk_m AS Harga_M, j.jk_l AS Harga_L,
       j.jk_xl AS Harga_XL, j.jk_2xl AS Harga_2XL, j.jk_3xl AS Harga_3XL,
       j.jk_4xl AS Harga_4XL, j.jk_5xl AS Harga_5XL
-      /* Tambahkan kolom harga lain jika ada (6XL, dst.) */
+      j.jk_6xl AS Harga_6XL, j.jk_7xl AS Harga_7XL, j.jk_8xl AS Harga_8XL,
+      j.jk_9xl AS Harga_9XL, j.jk_10xl AS Harga_10XL,
+      j.jk_oversize AS Harga_Oversize, j.jk_jumbo AS Harga_Jumbo
     FROM tjeniskaos j
     ORDER BY j.jk_Jenis, j.jk_custom DESC;
   `;
-  const [rows] = await pool.query(query);
-  return rows;
+    const [rows] = await pool.query(query);
+    return rows;
 };
 
 /**
@@ -52,7 +54,13 @@ const getTshirtTypeDetails = async (jenisKaos, custom) => {
         { ukuran: '3XL', harga: data.jk_3xl },
         { ukuran: '4XL', harga: data.jk_4xl },
         { ukuran: '5XL', harga: data.jk_5xl },
-        // Tambahkan ukuran lain jika ada
+        { ukuran: '6XL', harga: data.jk_6xl }, 
+        { ukuran: '7XL', harga: data.jk_7xl },
+        { ukuran: '8XL', harga: data.jk_8xl }, 
+        { ukuran: '9XL', harga: data.jk_9xl },
+        { ukuran: '10XL', harga: data.jk_10xl }, 
+        { ukuran: 'OVERSIZE', harga: data.jk_oversize },
+        { ukuran: 'JUMBO', harga: data.jk_jumbo }
     ];
 
     return {
@@ -79,20 +87,23 @@ const saveTshirtType = async (data) => {
         INSERT INTO tjeniskaos (
             jk_Jenis, jk_custom, 
             jk_s, jk_m, jk_l, jk_xl, jk_2xl, jk_3xl, jk_4xl, jk_5xl
-            /* Tambahkan jk_6xl, dst. di sini */
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? /* Tambahkan '?' untuk setiap kolom baru */)
+            jk_6xl, jk_7xl, jk_8xl, jk_9xl, jk_10xl, jk_oversize, jk_jumbo
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
             jk_s = VALUES(jk_s), jk_m = VALUES(jk_m), jk_l = VALUES(jk_l),
             jk_xl = VALUES(jk_xl), jk_2xl = VALUES(jk_2xl), jk_3xl = VALUES(jk_3xl),
-            jk_4xl = VALUES(jk_4xl), jk_5xl = VALUES(jk_5xl)
-            /* Tambahkan 'jk_6xl = VALUES(jk_6xl)', dst. di sini */
+            jk_4xl = VALUES(jk_4xl), jk_5xl = VALUES(jk_5xl),
+            jk_6xl = VALUES(jk_6xl), jk_7xl = VALUES(jk_7xl), jk_8xl = VALUES(jk_8xl),
+            jk_9xl = VALUES(jk_9xl), jk_10xl = VALUES(jk_10xl),
+            jk_oversize = VALUES(jk_oversize), jk_jumbo = VALUES(jk_jumbo)
     `;
 
     const params = [
         jenisKaos, custom,
         hargaMap['S'], hargaMap['M'], hargaMap['L'], hargaMap['XL'], 
-        hargaMap['2XL'], hargaMap['3XL'], hargaMap['4XL'], hargaMap['5XL']
-        // Tambahkan harga untuk ukuran lain di sini
+        hargaMap['2XL'], hargaMap['3XL'], hargaMap['4XL'], hargaMap['5XL'],
+        hargaMap['6XL'], hargaMap['7XL'], hargaMap['8XL'], hargaMap['9XL'],
+        hargaMap['10XL'], hargaMap['OVERSIZE'], hargaMap['JUMBO']
     ];
 
     await pool.query(query, params);
