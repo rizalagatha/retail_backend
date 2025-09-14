da12705d11506e1f6c2e1e7810d7f036a430b96f
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-09-14 10:23:49 +0700

    Buffer Stok

--- src/routes/bufferStockRoutes.js
+++ src/routes/bufferStockRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const bufferStockController = require('../controllers/bufferStockController');
+const verifyToken = require('../middleware/authMiddleware');
 
 router.post('/update', bufferStockController.update);
 
 // GET: Mengambil daftar Buffer Stok berdasarkan filter
-router.get('/', bufferStockController.getAll);
+router.get('/', verifyToken, bufferStockController.getAll);
 
 // GET: Mengambil daftar cabang untuk filter
-router.get('/lookup/cabang', bufferStockController.getCabangList);
+router.get('/lookup/cabang', verifyToken, bufferStockController.getCabangList);
 
 // POST: Menyimpan pengaturan Min/Max Buffer
-router.post('/setting', bufferStockController.saveSetting);
+router.post('/setting', verifyToken, bufferStockController.saveSetting);
 
 module.exports = router;
