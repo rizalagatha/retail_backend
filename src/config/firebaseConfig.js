4603b1ad264a581b8d878494077e52c340427610
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-08-20 19:03:36 +0700

    update

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -7,24 +7,24 @@ const serviceAccountPath = path.join(__dirname, "service-account.json");
 const apps = admin.apps || (admin.default && admin.default.apps) || [];
 
 if (!apps.length) {
-    if (fs.existsSync(serviceAccountPath)) {
-        try {
-            const serviceAccount = require("./service-account.json");
-            admin.initializeApp({
-                credential: admin.credential.cert(serviceAccount),
-            });
-            console.log("🔥 [FIREBASE] Berhasil diinisialisasi.");
-        } catch (err) {
-            console.warn(
-                "⚠️ [FIREBASE] Gagal menginisialisasi Firebase:",
-                err.message,
-            );
-        }
-    } else {
-        console.warn(
-            "⚠️ [FIREBASE] File service-account.json tidak ditemukan. Fitur Push Notification dinonaktifkan di lokal.",
-        );
+  if (fs.existsSync(serviceAccountPath)) {
+    try {
+      const serviceAccount = require("./service-account.json");
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+      console.log("🔥 [FIREBASE] Berhasil diinisialisasi.");
+    } catch (err) {
+      console.warn(
+        "⚠️ [FIREBASE] Gagal menginisialisasi Firebase:",
+        err.message,
+      );
     }
+  } else {
+    console.warn(
+      "⚠️ [FIREBASE] File service-account.json tidak ditemukan. Fitur Push Notification dinonaktifkan di lokal.",
+    );
+  }
 }
 
 module.exports = admin;
