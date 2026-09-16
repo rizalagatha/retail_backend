794332f44f50a725397bc639aaac6d697decd4e3
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-09-16 10:47:24 +0700

    1.0.9

--- src/services/returJualFormService.js
+++ src/services/returJualFormService.js
@@ -292,7 +292,7 @@ const lookupInvoices = async (cabang) => {
   // Tentukan batas waktu: 2 hari dari sekarang (Jan 29 s/d Jan 30)
   // Akses akan ditutup otomatis pada tanggal 31 Januari 2026
   const isTemporaryOpen =
-    cabang === "K05" && new Date() < new Date("2026-09-12");
+    cabang === "K01" && new Date() < new Date("2026-09-15");
 
   const dateCondition =
     cabang === "KON" || cabang === "KPR" || isTemporaryOpen
